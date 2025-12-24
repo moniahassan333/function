ba319eac3854719f984b6081e72f6cbc3ba7a90f
monia <https://github.com/moniahassan333>
2025-12-24 21:33:17 +0300

    remove bank folder

---
+++ bank/db.php
@@ -0,0 +1,15 @@
+<?php
+// الاتصال بقاعدة البيانات
+$host = "localhost";
+$db = "bank_app";  // اسم قاعدة البيانات
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("فشل الاتصال بقاعدة البيانات: " . $e->getMessage());
+}
+echo "connect";
+?>
